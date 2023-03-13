68b26ad0506ccb0d93bb7c8546688af21ccc43a0
ish0101 <75856466+ish0101@users.noreply.github.com>
2023-03-13 17:10:54 -0400

    Lab6 password encoder/decoder

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 # Ismael Maura
 # Aidan Wood
+# Lab6
+
 
 def encoder(password):
-    """
-    Encode an 8-digit password by shifting each digit up by 3 numbers.
-    """
+    # Encode an 8-digit password by shifting each digit up by 3 numbers.
     encoded_password = ""
     for digit in password:
         # Shift each digit up by 3 numbers and add to the encoded password
@@ -14,20 +14,36 @@ def encoder(password):
 
 
 def decoder(encoded_password):
-    """
-    Decode an encoded password by shifting each digit down by 3 numbers.
-    """
-    password = ""
-    for digit in encoded_password:
-        # Shift each digit down by 3 numbers and add to the original password
-        original_digit = str((int(digit) - 3) % 10)
-        password += original_digit
-    return password
+    pass
+
+
+def menu():
+    # Display a menu and prompt the user for an option.
+    print("\nMenu")
+    print("-------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit\n")
+    option = input("Please enter an option: ")
+    return option
 
 
 if __name__ == "__main__":
-
-    password = '12345555'
-    print(encoder(password))
-    print(decoder(encoder(password)))
-
+    encoded_password = None
+    while True:
+        option = menu()
+        if option == "1":
+            password = input("Please enter your password to encode: ")
+            encoded_password = encoder(password)
+            print("Your password has been encoded and stored!")
+        elif option == "2":
+            if encoded_password is None:
+                encoded_password = input("Please enter the encoded password: ")
+                password = decoder(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+            else:
+                password = decoder(encoded_password)
+                print(f"The encoded password is {encoded_password}, and the original password is {password}.")
+                encoded_password = None
+        elif option == "3":
+            exit()
